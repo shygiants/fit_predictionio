1c1a090cbaa159fbc122c0ff2b3bd59b6b7802e3
root <root@ip-172-31-5-168.ap-northeast-1.compute.internal>
2015-05-08 10:45:06 +0000

    First commit

---
+++ src/main/scala/Serving.scala
@@ -0,0 +1,13 @@
+package kr.ac.korea.ee.fit
+
+import io.prediction.controller.LServing
+
+class Serving
+  extends LServing[Query, PredictedResult] {
+
+  override
+  def serve(query: Query,
+    predictedResults: Seq[PredictedResult]): PredictedResult = {
+    predictedResults.head
+  }
+}
